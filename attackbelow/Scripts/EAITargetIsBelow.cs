75c9258631971021027a4eb0b8a0f6579b1efc96
Thirlan <marc.blomquist@gmail.com>
2018-08-17 00:03:01 -0400

    fixed distance bugs

--- attackbelow/Scripts/EAITargetIsBelow.cs
+++ attackbelow/Scripts/EAITargetIsBelow.cs
@@ -4,19 +4,56 @@ using UnityEngine;
 public class EAITargetIsBelow : EAIBase
 {
     private EntityAlive entityTarget;
-    private int waitCounter;
+    private DateTime lastAttemptTime;
+    private double waitTimeSeconds;
     private int itemActionType;
     private int attackPeriod;
     private int attackPeriodMax;
-	private float belowOffset;
-	private float minimumProximity;
+	private float minYDistanceBelow;
+	private float minimumDistance;
 
     public EAITargetIsBelow()
     {
-        this.waitCounter = 60;
+        this.lastAttemptTime = System.DateTime.UtcNow;
+        this.waitTimeSeconds = 7f;
         this.attackPeriodMax = 20;
-		this.belowOffset = 1f;
-		this.minimumProximity = 8f;
+		this.minYDistanceBelow = -2f;
+		this.minimumDistance = 64f; // radius^2
+    }
+
+	private bool IsInCircle(float xEntity, float zEntity, float xTarget, float zTarget)
+	{ 
+		float xDiff = xTarget - xEntity;
+		float zDiff = zTarget - zEntity;
+		float distanceSq = (xDiff*xDiff + zDiff*zDiff);
+		return distanceSq < this.minimumDistance;
+	}
+	
+    private bool IsBelow(float yEntity, float yTarget)
+    {
+        float yDiff = yTarget - yEntity;
+        return yDiff < this.minYDistanceBelow;
+    }
+	
+	private bool IsInRange(Vector3 entityVector, Vector3 targetVector)
+	{
+		return this.IsBelow(entityVector.y, targetVector.y)
+		&& this.IsInCircle(entityVector.x, entityVector.z, targetVector.x, targetVector.z);
+	}
+
+    private bool CanAttack()
+    {
+        if (!this.theEntity.Spawned)
+        {
+            return false;
+        }
+        else
+        {
+            this.entityTarget = this.theEntity.GetAttackTarget();
+            return this.entityTarget != null
+				&& this.entityTarget.IsAlive()
+				&& this.IsInRange(this.theEntity.GetPosition(), this.entityTarget.GetPosition());
+        }
     }
 
     public override void SetPar1(string _itemActionType)
@@ -29,71 +66,26 @@ public class EAITargetIsBelow : EAIBase
         this.attackPeriodMax = int.Parse(_attackPeriodMax) * 20;
     }
 
-    public bool IsBelow(float zEntity, float zTarget, float distanceSq)
-    {
-		if(((zEntity - belowOffset) > zTarget) && distanceSq < this.minimumProximity)
-		{
-			Debug.Log("zEntity: "+(zEntity - belowOffset)+" / zTarget: "+zTarget+" / distanceSq: "+distanceSq);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-    }
-
     public override bool CanExecute()
     {
-        if (this.waitCounter > 0)
+        DateTime currentTime = System.DateTime.UtcNow;
+		double elapsedSeconds = (currentTime - this.lastAttemptTime).TotalSeconds;
+        if ( elapsedSeconds < this.waitTimeSeconds)
         {
-            this.waitCounter--;
             return false;
         }
-        if (!this.theEntity.Spawned
-            || !this.theEntity.IsAttackValid())
-        {
-			Debug.Log("CanExecute: Not spawned or !IsAttackValid");
-            return false;
-        }
-        else
-        {
-            this.entityTarget = this.theEntity.GetAttackTarget();
-            if (this.entityTarget == null) {
-                return false;
-            }
-            float distanceSq = this.entityTarget.GetDistanceSq(this.theEntity);
-            
-			if(this.IsBelow(this.theEntity.GetPosition().z,
-                                this.entityTarget.GetPosition().z,
-                                distanceSq))
-			{
-				Debug.Log("CanExecute: "+this.entityTarget);
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-        }
+        this.lastAttemptTime = currentTime;
+        return CanAttack();
     }
     
     public override void Start()
     {
-		Debug.Log("Start");
         this.attackPeriod = this.attackPeriodMax;
     }
 
     public override bool Continue()
     {
-        if (this.entityTarget == null
-            || !this.entityTarget.IsAlive()
-            || this.attackPeriod <= 0)
-        {
-			Debug.Log("Continue: abort");
-            return false;
-        }
-
-        return true;
+        return CanAttack() && this.attackPeriod > 0;
     }
 
     public override void Update()
@@ -101,11 +93,10 @@ public class EAITargetIsBelow : EAIBase
         this.attackPeriod--;
         if((float)this.attackPeriod > (float)this.attackPeriodMax * 0.5f)
         {
-			Debug.Log("Update");
-            //if(this.theEntity.IsInFrontOfMe(this.entityTarget.getHeadPosition()))
-            //{
+            if(this.theEntity.IsInFrontOfMe(this.entityTarget.getHeadPosition()))
+            {
                 this.theEntity.SetLookPosition(this.entityTarget.getHeadPosition());
-            //}
+            }
             if(this.itemActionType == 0)
             {
                 this.theEntity.Attack(this.attackPeriod == 0);
@@ -119,18 +110,17 @@ public class EAITargetIsBelow : EAIBase
 
     public override void Reset()
     {
-		Debug.Log("Reset");
-        //this.entityTarget = null;
+        this.entityTarget = null;
         this.attackPeriod = 0;
-        this.waitCounter = 30 + UnityEngine.Random.Range(20, 50);
+        this.waitTimeSeconds = 6f + (float)UnityEngine.Random.Range(1, 4);
 
-        //if (this.itemActionType == 0)
-        //{
-        //    this.theEntity.Attack(true);
-        //}
-        //else
-        //{
-        //    this.theEntity.Use(true);
-        //}
+        if (this.itemActionType == 0)
+        {
+            this.theEntity.Attack(true);
+        }
+        else
+        {
+            this.theEntity.Use(true);
+        }
     }
 }
